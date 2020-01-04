fa62290925a9cef57a1647da1cc621347607821c
Neil Turley <neilpturley@gmail.com>
2020-01-04 09:14:45 -0600

    fix mqtt connect call

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 
-const express = require('express')
-var mqtt = require('mqtt'), url = require('url');
+const express = require('express');
+const mqtt = require('mqtt');
+const url = require('url');
 // Parse
-var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
-var auth = (mqtt_url.auth || ':').split(':');
+const mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
+const auth = (mqtt_url.auth || ':').split(':');
 
-
-
+var client  = mqtt.connect(mqtt_url, [{username: auth0, password: auth1}])
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    // Create a client connection
-    var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
-        username: auth[0],
-        password: auth[1]
+app.get('/', (req, res) => { 
+    client.publish('hello/world', 'my message', function() {
+        console.log("Message is published");
     });
-    client.on('connect', function() { // When connected      
-        // publish a message to a topic
-        client.publish('hello/world', 'my message', function() {
-          console.log("Message is published");
-          client.end(); // Close the connection when published
-        });
-      });
-  
     res.send('Hello World!');
 });
 
