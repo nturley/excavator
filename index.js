c6f2f815878290d2c29fe3b187e87d47b65bc1c7
Neil Turley <neilpturley@gmail.com>
2020-01-05 12:06:24 -0600

    get crud working

--- index.js
+++ index.js
@@ -1,21 +1,100 @@
-
 const express = require('express');
-const mqtt = require('mqtt');
-const url = require('url');
-// Parse
-const mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
-const auth = (mqtt_url.auth || ':').split(':');
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
 
-var client  = mqtt.connect(mqtt_url, [{username: auth[0], password: auth[1]}])
 const app = express()
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => { 
-    client.publish('hello/world', 'my message', function() {
-        console.log("Message is published");
-    });
+var CONTACTS_COLLECTION = "contacts";
+
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({ "error": message });
+}
+
+app.get('/', (req, res) => {
+    console.log('get!')
     res.send('Hello World!');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/api/contacts', (req, res) => {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function (err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+});
 
+// CREATE
+app.post("/api/contacts", (req, res) => {
+    const newContact = req.body;
+    newContact.createDate = new Date();
+
+    if (!newContact.name) {
+        handleError(res, "Invalid user input", "Must provide a name.", 400);
+    } else {
+        db.collection(CONTACTS_COLLECTION).insertOne(newContact, (err, doc) => {
+            if (err) {
+                handleError(res, err.message, "Failed to create new contact.");
+            } else {
+                res.status(201).json(doc.ops[0]);
+            }
+        });
+    }
+});
+
+// RETRIEVE
+app.get("/api/contacts/:id", function (req, res) {
+    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contact");
+        } else {
+            res.status(200).json(doc);
+        }
+    });
+});
+
+// UPDATE
+app.put("/api/contacts/:id", function (req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+
+    db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to update contact");
+        } else {
+            updateDoc._id = req.params.id;
+            res.status(200).json(updateDoc);
+        }
+    });
+});
+// DELETE
+app.delete("/api/contacts/:id", function (req, res) {
+    db.collection(CONTACTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+        if (err) {
+            handleError(res, err.message, "Failed to delete contact");
+        } else {
+            res.status(200).json(req.params.id);
+        }
+    });
+});
+
+
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    db = client.db();
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    var server = app.listen(port, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
