5b7b77308a4b93a7e118ef38e66010146477b482
Neil Turley <neilpturley@gmail.com>
2020-01-05 12:13:48 -0600

    add missing variable

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000
 
+var ObjectID = mongodb.ObjectID;
 var CONTACTS_COLLECTION = "contacts";
 
 function handleError(res, reason, message, code) {
